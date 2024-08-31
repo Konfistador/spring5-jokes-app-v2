fecc72cfa4c826c329fb448f74c17543a93503af
konfistador <m.karadjov@icloud.com>
2024-08-31 20:12:28 +0300

    Spring boot installation added

---
+++ src/test/java/com/mino/jokesApp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mino.jokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
