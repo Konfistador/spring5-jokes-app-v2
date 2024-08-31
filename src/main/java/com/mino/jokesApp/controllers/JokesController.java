61a28e9a02633f6f73c5e921bc68cfdb410aaf74
konfistador <m.karadjov@icloud.com>
2024-08-31 23:02:24 +0300

    Added Controller for Jokes. Removed redundant model. Closes #4

---
+++ src/main/java/com/mino/jokesApp/controllers/JokesController.java
@@ -0,0 +1,23 @@
+package com.mino.jokesApp.controllers;
+
+import com.mino.jokesApp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    public String getJoke(Model model){
+       String retrievedJoke =  jokeService.getRandomQuote();
+        model.addAttribute("joke", retrievedJoke);
+        return "index";
+    }
+}
